8e45e01c8d6b9756d3c0b85e902e97f2be6c8953
MMA <58857539+TheGeeKing@users.noreply.github.com>
2025-07-31 21:22:13 +0200

    fix casing

--- app/(drawer)/itinerary/index.tsx
+++ app/(drawer)/itinerary/index.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function itinerary() {
+export default function Itinerary() {
   return (
-    <View className='flex flex-row justify-center items-center my-auto'>
-      <Text className='font-bold text-6xl'>Itinéraires</Text>
+    <View className="flex flex-row justify-center items-center my-auto">
+      <Text className="font-bold text-6xl">Itinéraires</Text>
     </View>
-  )
+  );
 }
